1c7977e599994538bc98c4acd93ceb9c9b99ec7c
unknown <239027@TECH.WYB.AC.LK>
2025-03-26 18:43:12 +0530

    first commit

--- test1.py
+++ test1.py
@@ -1,1 +1,8 @@
 print("this is my first python programm")
+print("this is update")
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Chandimabandara03/python-workshop.git
+git push -u origin main
